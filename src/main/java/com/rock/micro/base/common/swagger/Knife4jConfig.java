0202ae86686b6ec1d9a9eced56b38cd73a904eac
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-18 14:23:36 +0800

    修改：knife4j-2

--- src/main/java/com/rock/micro/base/common/swagger/Knife4jConfig.java
+++ src/main/java/com/rock/micro/base/common/swagger/Knife4jConfig.java
@@ -25,19 +25,19 @@ public class Knife4jConfig {
         return new Docket(DocumentationType.OAS_30)
                 .apiInfo(new ApiInfoBuilder()
                         //标题
-                        .title("com.rock.api.document")
+                        .title("com.rock.micro.api.document")
                         //描述
-                        .description("这是base基底的接口文档.")
+                        .description("微服务接口文档.")
                         //联系人
                         .contact(new Contact("rock", "com.rock", "Rock-Ayl@foxmail.com"))
                         //版本
-                        .version("1.0.0.2")
+                        .version("1.0.0.1")
                         .build())
                 //分组名称(也可以理解为版本)
                 .groupName("1.0")
                 //选择哪些接口作为swagger的doc发布
                 .select()
-                //这里指定Controller扫描包路径
+                //这里指定Controller扫描包路径,这里是所有包的总路径
                 .apis(RequestHandlerSelectors.basePackage("com.rock.micro"))
                 .paths(PathSelectors.any())
                 .build();
