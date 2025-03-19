9b99a3db86bd7894a22b0bdff59eac4517f69218
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-19 09:57:50 +0800

    修改：mybatis配置

---
+++ src/main/java/com/rock/micro/base/common/mybatis/MyBatisConfig.java
@@ -0,0 +1,26 @@
+package com.rock.micro.base.common.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 手动配置SqlSessionFactory
+ *
+ * @Author ayl
+ * @Date 2025-03-19
+ */
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
+    }
+
+}
--- src/main/java/com/rock/micro/base/common/redis/RedisConfig.java
+++ src/main/java/com/rock/micro/base/common/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.rock.micro.base.db.redis;
+package com.rock.micro.base.common.redis;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
