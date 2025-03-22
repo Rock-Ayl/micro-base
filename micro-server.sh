3fe01b726ab68c3d1ad93bc478cb24d4f235b6a4
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-22 10:26:42 +0800

    修改：脚本

---
+++ micro-server.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#包名
+APP_NAME="micro-user-1.0.0-SNAPSHOT.jar"
+#包路径
+APP_PATH="$(pwd)/${APP_NAME}"
+
+#进程PID
+pid=0
+
+# 帮助
+Help() {
+    # 输出信息
+    echo "[启动=start|停止=stop|重启=restart|查看状态=status]"
+    echo "请类似这样执行 ./*.sh start"
+    exit 1
+}
+
+# 检查pid
+checkPID() {
+   # 查询pid
+   pid=$(pgrep -f "$APP_NAME")
+}
+
+
+# 查询状态
+status() {
+   # 检查pid
+   checkPID
+   # 判断是否存在
+   if [ -z "$pid" ]; then
+      # 不存在
+      echo "目前不存在该进程."
+   else
+      echo "pid=${pid}"
+   fi
+}
+
+# 停止
+stop(){
+    # 检查pid
+    checkPID
+    # 判断是否存在pid
+    if [ -n "$pid" ]; then
+        # 立刻杀死线程
+        kill -9 $pid
+        # 再次检查确保进程终止
+        if pgrep -f "$APP_NAME" > /dev/null; then
+            # 停止失败
+            echo "警告：进程 ${pid} 停止失败！"
+        else
+            # 停止成功
+            echo "${pid} 进程已成功终止."
+        fi
+    else
+        # 不存在任何进程
+        echo "${APP_NAME} 没有启动."
+    fi
+}
+
+# 启动
+start(){
+    # 检查pid
+    checkPID
+    # pid存在,则不启动,直接输出
+    if [ -n "$pid" ]; then
+        # 输出启动信息
+        echo "${APP_NAME} 已经启动,PID:${pid}"
+    else
+        # 执行java -jar 正常启动服务
+        nohup java -jar $APP_NAME > /dev/null 2>&1 &
+        # 检查pid
+        checkPID
+        # 输出
+        echo "${APP_NAME} 启动成功."
+        echo "${APP_NAME} PID:${pid}"
+    fi
+}
+
+# 重启
+restart(){
+    stop
+    start
+}
+
+# 命令表
+case "$1" in
+    "start")
+        start
+        ;;
+    "stop")
+        stop
+        ;;
+    "restart")
+        restart
+        ;;
+
+    "status")
+        status
+        ;;
+    *)
+    Help
+    ;;
+esac
