2dcc9df2e2b61666d8428feef11ef5473f8ace5c
Rock-Ayl <AnYongLiang4869@163.com>
2025-12-08 16:47:19 +0800

    修改：异常枚举优化

--- src/main/java/com/rock/micro/base/common/api/MyException.java
+++ src/main/java/com/rock/micro/base/common/api/MyException.java
@@ -3,6 +3,8 @@ package com.rock.micro.base.common.api;
 import com.rock.micro.base.util.LambdaParseFieldNameExtraUtils;
 import lombok.Getter;
 
+import java.util.List;
+
 /**
  * 系统统一封装,运行时异常
  */
@@ -12,13 +14,16 @@ public class MyException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     //非通用-开发内部-中文描述
-    public String zhDesc;
+    private String zhDesc;
 
     //通用-对用户-国际化异常枚举
-    public MyExceptionEnum myExceptionEnum;
+    private MyExceptionEnum myExceptionEnum;
 
-    //通用-对用户-抛出时返回错误的key
-    public String errorKey;
+    //通用-对用户-抛出时返回错误的 key
+    private String errorKey;
+
+    //通用-对用户-抛出时返回错误的 值列表
+    private List<String> errorValueList;
 
     /**
      * 通用-抛给用户,需要做国际化的异常(如密码格式、邮箱格式等错误)
@@ -41,6 +46,19 @@ public class MyException extends RuntimeException {
     public <T, R> MyException(MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey) {
         this(myExceptionEnum);
         this.errorKey = LambdaParseFieldNameExtraUtils.getColumn(errorKey);
+    }
+
+    /**
+     * 通用-抛给用户,需要做国际化的异常(如密码格式、邮箱格式等错误),额外返回错误key
+     *
+     * @param myExceptionEnum 异常枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public <T, R> MyException(MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        this(myExceptionEnum);
+        this.errorKey = LambdaParseFieldNameExtraUtils.getColumn(errorKey);
+        this.errorValueList = errorValueList;
     }
 
     /**
