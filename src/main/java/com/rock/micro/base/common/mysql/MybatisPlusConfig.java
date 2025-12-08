1f92fdf97f4509886d56dabb69d41edddb6307fb
Rock-Ayl <AnYongLiang4869@163.com>
2025-12-08 17:18:38 +0800

    修改：mysql配置优化

---
+++ src/main/java/com/rock/micro/base/common/mysql/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.rock.micro.base.common.mysql;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis plus 插件配置
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 让 mybatis plus 分页参数生效
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
