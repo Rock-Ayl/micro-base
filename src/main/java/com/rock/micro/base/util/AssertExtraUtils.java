a5fb3796aa064d921e64142a4b1967d6335d3772
Rock-Ayl <AnYongLiang4869@163.com>
2026-08-25 17:18:38 +0800

    修改：抛出扩展-1

--- src/main/java/com/rock/micro/base/util/AssertExtraUtils.java
+++ src/main/java/com/rock/micro/base/util/AssertExtraUtils.java
@@ -6,6 +6,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * 通用断言类
@@ -299,6 +300,115 @@ public class AssertExtraUtils {
         if (CollectionUtils.isEmpty(collection) || collection.size() < 1) {
             //抛出
             throw new MyException(myExceptionEnum, errorKey);
+        }
+    }
+
+    /**
+     * 一定抛出
+     *
+     * @param myExceptionEnum 要抛出的枚举
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public static <T, R> void assertFail(MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //抛出
+        throw new MyException(myExceptionEnum, errorKey, errorValueList);
+    }
+
+    /**
+     * 坚持为 true
+     *
+     * @param bool            传入参数
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public static <T, R> void assertTrue(boolean bool, MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //判断
+        if (bool == false) {
+            //抛出
+            throw new MyException(myExceptionEnum, errorKey, errorValueList);
+        }
+    }
+
+    /**
+     * 坚持为 false
+     *
+     * @param bool            传入参数
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表ø
+     */
+    public static <T, R> void assertFalse(boolean bool, MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //判断
+        if (bool == true) {
+            //抛出
+            throw new MyException(myExceptionEnum, errorKey, errorValueList);
+        }
+    }
+
+    /**
+     * 坚持不为 null
+     *
+     * @param obj             传入参数
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public static <T, R> void assertNotNull(Object obj, MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //判断
+        if (obj == null) {
+            //抛出
+            throw new MyException(myExceptionEnum, errorKey, errorValueList);
+        }
+    }
+
+    /**
+     * 坚持不为 Empty
+     *
+     * @param obj             传入参数
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public static <T, R> void assertNotEmpty(String obj, MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //判断
+        if (StringUtils.isEmpty(obj)) {
+            //抛出
+            throw new MyException(myExceptionEnum, errorKey, errorValueList);
+        }
+    }
+
+    /**
+     * 坚持不为 Blank
+     *
+     * @param obj             传入参数
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public static <T, R> void assertNotBlank(String obj, MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //判断
+        if (StringUtils.isBlank(obj)) {
+            //抛出
+            throw new MyException(myExceptionEnum, errorKey, errorValueList);
+        }
+    }
+
+    /**
+     * 坚持不为 Empty
+     *
+     * @param collection      集合
+     * @param myExceptionEnum 要抛出的枚举
+     * @param errorKey        错误key
+     * @param errorValueList  错误的值列表
+     */
+    public static <T, R> void assertNotEmpty(Collection collection, MyExceptionEnum myExceptionEnum, LambdaParseFieldNameExtraUtils.MFunction<T, R> errorKey, List<String> errorValueList) {
+        //判空
+        if (CollectionUtils.isEmpty(collection) || collection.size() < 1) {
+            //抛出
+            throw new MyException(myExceptionEnum, errorKey, errorValueList);
         }
     }
 
